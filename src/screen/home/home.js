35e88e02afeac75b65194fff85e9cf331610f5c1
mfadel26 <muhammadfadelp@gmail.com>
2020-12-30 11:02:12 +0700

    delete

--- src/screen/home/home.js
+++ src/screen/home/home.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react'
+import  {Link} from 'react-router-dom'
 const axios = require('axios')
 function Home() {
   // Declare a new state variable, which we'll call "count"
